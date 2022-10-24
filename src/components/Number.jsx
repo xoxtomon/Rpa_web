b4834dfb2e3ecec274dc71368702678fcc1b2e0c
xoxtomon <xoxtomon@hotmail.com>
2022-10-24 17:55:14 -0500

    delay 4 proof of concept

--- src/components/Number.jsx
+++ src/components/Number.jsx
@@ -5,10 +5,14 @@ function Number(props) {
     return (
         <div class="card-title cstm">
             <CountUp
+                start={0}
                 end={props.end}
+                delay={7}
                 duration={0.75}
                 suffix="%"
-                enableScrollSpy={true} />
+                enableScrollSpy={true}
+                onEnd={() => console.log('Ended! 👏')}
+                onStart={() => console.log('Started! 💨')} />
         </div>
     );
 }
