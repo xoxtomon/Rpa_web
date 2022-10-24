8f0e60a821ac3c830f618ef8c29d77499687d7e6
xoxtomon <xoxtomon@hotmail.com>
2022-10-24 18:18:13 -0500

    reveting to 99568b970a2cb49ae7b137655fac5dade308e962

--- src/components/Number.jsx
+++ src/components/Number.jsx
@@ -5,14 +5,11 @@ function Number(props) {
     return (
         <div class="card-title cstm">
             <CountUp
-                start={0}
                 end={props.end}
-                delay={7}
                 duration={0.75}
                 suffix="%"
-                enableScrollSpy={true}
-                onEnd={() => console.log('Ended! 👏')}
-                onStart={() => console.log('Started! 💨')} />
+                enableScrollSpy={false}
+                delay={15} />
         </div>
     );
 }
